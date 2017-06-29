09c2bd62214d366e8c5d98d7abfdffda0e966abc
kks35 <zkak21@gmail.com>
2017-06-28 22:57:55 -0400

    Add files via upload

--- udp1.cpp
+++ udp1.cpp
@@ -4,29 +4,50 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <iostream>
 
+using namespace std
 
 #define port 8888
 int main(){
     //create socket
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
     char buffer[2048];
+
+    if(client < 0)
+    {
+        cout<<"error creating sockets..." << endl;
+        exit(1)
+    }
     //send request to server
     struct sockaddr_in serv_addr;//began to binding the socket
     memset(&serv_addr, 0, sizeof(serv_addr));  //0zero for each byte
     serv_addr.sin_family = AF_INET;  //use IPv4
     serv_addr.sin_addr.s_addr = inet_addr("192.168.1.177");  //IP
     serv_addr.sin_port = htons(port);  //port
-    connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    {
+        cout<<"Connecting to server..." <<endl;
+    }
+
 
     //read back data
     for (;;) {
-                printf("waiting on port %d\n", port);
-                recvlen = recvfrom(sock, buffer, sizeof(buffer), 0, (struct sockaddr *)&remaddr, &addrlen);
+                struct sockaddr_in remaddr;
+                unasigned addrlen = sizeof(remaddr);
+
+                unsigned char buff="X_out";
+                write(sock,buff,sizeof(buff));
+
+
+
+                int recvlen = recvfrom(sock, buffer, sizeof(buffer), 0, (struct sockaddr *)&remaddr, &addrlen);
                 printf("received %d bytes\n", recvlen);
                 if (recvlen > 0) {
                         buf[recvlen] = 0;
-                        printf("RSSI: \"%d\"\n", buf)
+                        printf("RSSI: \"%d\"\n", buf);
+                }
+    }
 
 
     close(sock);
