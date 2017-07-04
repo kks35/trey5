bd825817da9179facc0a9493599839c4dd6baa1a
kks35 <zkak21@gmail.com>
2017-07-03 23:27:56 -0400

    Add files via upload

---
+++ udp2.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <iostream>
+
+using namespace std;
+
+#define port 8888
+
+int main(){
+    //create socket
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+    char buffer[64];
+
+    //send request to server
+    struct sockaddr_in serv_addr;//began to binding the socket
+    memset(&serv_addr, 0, sizeof(serv_addr));  //0zero for each byte
+    serv_addr.sin_family = AF_INET;  //use IPv4
+    serv_addr.sin_addr.s_addr = inet_addr("192.168.1.177");  //IP
+    serv_addr.sin_port = htons(port);  //port
+    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)))
+    {
+        cout<<"Connecting to server..." <<endl;
+    }
+
+    unsigned char buff[]="X_out";
+    bool xout_sent = false;
+
+    //read back data
+    for (;;) {            
+        if (!xout_sent) {
+            write(sock,buff,sizeof(buff));
+            xout_sent = true;
+        }
+
+        int recvlen = recvfrom(sock, buffer, sizeof(buffer), 0, NULL, NULL);
+        // printf("received %d bytes\n", recvlen);
+        if (recvlen > 0) {
+            buffer[recvlen] = 0;
+            printf("RSSI: \"%s\"\n", buffer);
+        }
+    }
+
+
+    close(sock);
+    return 0;
+}
