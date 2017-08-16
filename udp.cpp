2f14798d309048c85d47092b31af8fea4335a81c
kks35 <zkak21@gmail.com>
2017-08-15 22:22:33 -0400

    Update udp.cpp

--- udp.cpp
+++ udp.cpp
@@ -1,9 +1,10 @@
-
 // KKS Ethernet UDP connection
 #include <SPI.h> // SPI library
 #include <Ethernet2.h>
 #include <EthernetUdp2.h>         // UDP library 
 #include <Wire.h> // for i2c
+
+#include <time.h>
 
 /////////////////////////////////////////////////////////////// i2c reading address/////////////////
 int timberconAddress = 0x51; // device address
@@ -20,7 +21,10 @@ String datReq; //String for our data
 int packetSize;// Size of the packet
 EthernetUDP Udp; // Create UDP Object
 
+
 char  ReplyBuffer[] = "acknowledged";       // a string to send back
+//////////////////////////////////////////
+char  clockStr[9]; // set time stamp
 
 void setup() {
   // start the Ethernet and UDP:
@@ -37,22 +41,23 @@ void setup() {
 }
 
 void loop() {
+    int packetSize = Udp.parsePacket();// if there's data available, read a packet
+    if (packetSize>0) {     //if packet size>0 , some one is asking data
+      Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE); // Read data request
+      String datReq(packetBuffer); // convert char array into a string we called -- dataRequest
+      if(datReq == "X_out"){ // do the following i2c
+      }
+    }
+  
 
- // int packetSize = Udp.parsePacket();// if there's data available, read a packet //delete
- // if (packetSize>0) {     //if packet size>0 , some one is asking data //delete
- //   Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE); // Read data request //delete
- //   String datReq(packetBuffer); // convert char array into a string we called -- dataRequest //delete
-
- //   if(datReq == "X_out"){ // do the following i2c// try to remove //delete
-
+    
 ////////////////////////i2C reading from TimberCon//////////////
   Wire.beginTransmission(timberconAddress);
   Wire.write(rssi_Register_DATA_ADDR);
   Wire.endTransmission(); 
   Wire.requestFrom(timberconAddress, 2);    // request 2 bytes from timbercon #2
 
-  if (Wire.available()<=2) { // 
-    
+  if (Wire.available()>=2) { // 
     X0 = Wire.read();
     X1 = Wire.read();
     X_out = X0*256+X1;
@@ -60,10 +65,5 @@ void loop() {
     Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
     Udp.print(X_out);
     Udp.endPacket();
- 
-//}
-//}
   }
 }
-
-
